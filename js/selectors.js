92364458660908695aa81964102e6a79913a5cd7
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-08 07:21:39 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,12 @@ return  $('p')
 function lastImageSelector(){
   return $('img:last')
 }
+function ninjaBabySelector(){
+  $('#baby-ninja')
+}
+function divSelector(){
+  return $('.pics')
+}
+function firstListIten(){
+  $('#pic-list ul:first-child')
+}
